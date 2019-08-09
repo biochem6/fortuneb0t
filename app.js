5bebea2f633ef44ea440574ad52776ba8698818b
biochem6 <aashland5@gmail.com>
2019-08-09 15:14:31 -0400

    .

---
+++ app.js
@@ -0,0 +1,66 @@
+
+console.log("hello")
+const tmi = require("tmi.js");
+const fetch = require('node-fetch');
+
+const options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
+    identity: {
+        username: "fortuneb0t",
+        password: "oauth:ibxu99jq1agxkydex1dj2now4vufoq"
+    },
+    channels: ["fortuneb0t", "phoodd"]
+};
+
+const client = new tmi.client(options);
+
+client.connect();
+
+(function chat() {
+    client.on("chat", function (channel, user, message, self, accessLvl) {
+    
+        if (self) return;
+
+        if(message === "!fortune") {
+            fetch('http://fortunecookieapi.herokuapp.com/v1/fortunes').then(res => res.json()).then(data => client.action(channel, data[Math.floor((Math.random() * 100) + 1)].message));
+        } else if(message === "!lesson") {
+            fetch(`http://fortunecookieapi.herokuapp.com/v1/lessons/`).then(res => res.json())
+                                                .then(data => {                           
+                                                    var rsp = data[Math.floor((Math.random() * 100) + 1)];                                                   
+                                                    response =  "English: " + rsp.english + 
+                                                                "*******" + 
+                                                                "Chinese: " + rsp.chinese +
+                                                                "*******" +
+                                                                "Pronunciation: " + rsp.pronunciation
+                                                    client.action(channel, response)                            
+                                                    
+                                                });        
+        } else if (message === "!lotto") {
+            function getRandom() {
+                return Math.floor((Math.random() * 99) + 1)
+            }
+
+            var lottoArr = [
+                getRandom(),
+                getRandom(),
+                getRandom(),
+                getRandom(),
+                getRandom(),
+                getRandom(),
+                getRandom(),
+            ];
+
+            var lottoResponse = `Your winning lottery numbers are: ${lottoArr}`;
+            client.action(channel, lottoResponse);
+        }
+    });
+})();
+
+
+
+
